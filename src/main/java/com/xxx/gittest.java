4377e91c4310b8313ff6db52953d54f89541b326
Xzzz <diavd_455899952@qq.com>
2021-07-21 14:13:24 +0800

    git-push commit

--- src/main/java/com/xxx/gittest.java
+++ src/main/java/com/xxx/gittest.java
@@ -6,7 +6,6 @@ public class gittest {
         System.out.println("第二次提交");
         System.out.println("你好java");
         System.out.println("你好git");
-        System.out.println("你好git");
         System.out.println("push-test");
     }
 }
